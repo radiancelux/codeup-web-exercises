dbb95fb5f7d07a660af6a7febb940c5aa8b9b499
Brett Humphreys <radiancelux@gmail.com>
2023-11-07 12:02:45 -0600

    added price and minor style improvements

--- js/pizza.js
+++ js/pizza.js
@@ -13,25 +13,57 @@ function navigateSection(sectionId) {
         tab.classList.toggle('active', index === targetTabIndex);
     });
 }
+
+// Define prices
+const sizePrices = {
+    small: 10,
+    medium: 12,
+    large: 14,
+};
+
+const crustPrices = {
+    thin: 0,
+    thick: 2,
+};
+
+const cheesePrices = {
+    regular: 0,
+    extra: 1,
+    double: 2,
+};
+
+const toppingPrices = {
+    pepperoni: 1,
+    mushrooms: 2,
+    onions: 1,
+    // Add more toppings and prices as needed
+};
+
+
 function placeOrder() {
     // Gather selected options from each section
     const size = document.getElementById('pizzaSize').value;
     const crust = document.querySelector('input[name="crustOptions"]:checked').value;
     const cheese = document.getElementById('cheese').value;
-    const sauce = document.getElementById('sauce').value;
     const toppings = [];
     document.querySelectorAll('input[type="checkbox"]:checked').forEach(checkbox => {
         toppings.push(checkbox.value);
+    });
+
+    // Calculate the total based on selected options and prices
+    let total = sizePrices[size] + crustPrices[crust] + cheesePrices[cheese];
+    toppings.forEach(topping => {
+        total += toppingPrices[topping];
     });
 
     // Set the order details in the modal
     document.getElementById('modalSize').textContent = size;
     document.getElementById('modalCrust').textContent = crust;
     document.getElementById('modalCheese').textContent = cheese;
-    document.getElementById('modalSauce').textContent = sauce;
     document.getElementById('modalToppings').textContent = toppings.join(', ');
+    document.getElementById('modalTotal').textContent = `$${total.toFixed(2)}`;
 
     // Show the modal
     const orderModal = new bootstrap.Modal(document.getElementById('orderModal'));
     orderModal.show();
-};
+}
